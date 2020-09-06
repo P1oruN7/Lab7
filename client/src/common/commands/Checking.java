148b05b72a7e65072ebfc0dfc2d8aca6226cdfbc
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-09-06 23:47:01 +0300

    Add files via upload

---
+++ client/src/common/commands/Checking.java
@@ -0,0 +1,17 @@
+package common.commands;
+
+import common.Command;
+
+/**
+ * Метод проверки (нужен на серваке)
+ */
+public class Checking implements Command {
+    @Override
+    public void execute(String par1) {
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+}
