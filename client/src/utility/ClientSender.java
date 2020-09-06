3f662ff9b449d7818d537aea44106409e0719292
Olya-droid <kragway@gmail.com>
2020-09-07 04:38:31 +0500

    это так должно выглядеть?

--- client/src/utility/ClientSender.java
+++ client/src/utility/ClientSender.java
@@ -16,18 +16,28 @@ public class ClientSender {
      */
     public static void send(Object o) {
         try {
-            Map<Integer, Object> map = new HashMap<Integer, Object>();
+            Map<Integer, Object> mapCommand = new HashMap<Integer, Object>();
+            Map<Integer, Object> mapUser = new HashMap<Integer, Object>();
+
             DatagramChannel datagramChannel = DatagramChannel.open();
             datagramChannel.bind(null);
             SocketAddress serverAddress = new InetSocketAddress(ClientMain.address, ClientMain.port);
+
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-            map.put(ClientReceiver.clientport, o);
-            objectOutputStream.writeObject(map);
+
+            mapCommand.put(ClientReceiver.clientport, o);
+            objectOutputStream.writeObject(mapCommand);
             objectOutputStream.flush();
+
+            mapUser.put(ClientReceiver.clientport,) //?? тут на вход менять на два параметра как то неоч хочется ъеъ
+            objectOutputStream.writeObject(mapUser);
+            objectOutputStream.flush();
+
             objectOutputStream.close();
             byte[] buff = byteArrayOutputStream.toByteArray();
             byteArrayOutputStream.close();
+
             datagramChannel.configureBlocking(false);
             datagramChannel.send(ByteBuffer.wrap(buff), serverAddress);
             datagramChannel.close();
