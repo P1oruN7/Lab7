b1a42746730c1a6413276400cabbaefe1a309a4f
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-09-08 21:24:37 +0300

    Update UserCheck.java

--- server/src/users/UserCheck.java
+++ server/src/users/UserCheck.java
@@ -4,7 +4,7 @@ import utility.Hash;
 import utility.ServerMain;
 
 public class UserCheck {
-    public boolean loginIsExist(String login){
+    public static boolean loginIsExist(String login){
         boolean isExist = false;
         isExist = ServerMain.users.searchByLogin(login) != null;
         return isExist;
@@ -12,11 +12,11 @@ public class UserCheck {
 
 
 
-    public boolean isThisRouteYours (String login, Long ObjectID){
+    public static boolean isThisRouteYours (String login, Long ObjectID){
         return ServerMain.c.searchById(ObjectID).getCreatorLogin().equals(login); //странный метод какой-то это ок ваще не
     }
 
-    public boolean correctPassword (String login, String password){
+    public static boolean correctPassword (String login, String password){
         boolean isPasswordCorrect = false;
         User u = ServerMain.users.searchByLogin(login);
         if (u != null) isPasswordCorrect = u.getPassword().equals(Hash.encryptThisString(password));
