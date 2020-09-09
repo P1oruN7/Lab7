c3d147f58ce53344d39407c4bd0a0eb4cca09e37
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-09-10 02:20:08 +0300

    Update Checking.java

--- server/src/common/commands/Checking.java
+++ server/src/common/commands/Checking.java
@@ -13,6 +13,7 @@ import java.net.*;
  * Метод проверки (нужен на серваке)
  */
 public class Checking implements Command {
+    private static final long serialVersionUID = 6529685098267757690L;
     @Override
     public void execute(String string, SocketAddress clientAddress) {
         Boolean b;
